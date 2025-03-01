6265708d9f9e666f0866d3b6e94e90ac9eeb73db
halitayhanaydin@icloud.com <halitayhanaydin@icloud.com>
2025-03-01 13:57:06 +0300

    Initial Commit

---
+++ AuthLoginRegister/AuthLoginRegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthLoginRegisterApp.swift
+//  AuthLoginRegister
+//
+//  Created by Halit Ayhan Aydın on 1.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthLoginRegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
